061b410eb15f0787b9eab9b1eb805eef7cc4c8f8
jberg <jordannealberg@gmail.com>
2018-08-22 16:20:16 -0700

    fix glsl-optimizer loading

--- src/convert.worker.js
+++ src/convert.worker.js
@@ -5,7 +5,7 @@ import {
   createBasePresetFuns
 } from 'milkdrop-preset-utils';
 import milkdropParser from 'milkdrop-eel-parser';
-import optimizeGLSL from './glslOptimizer';
+import GLSLOptimizer from 'glsl-optimizer-js';
 
 const CONVERT_URL =
   'https://p2tpeb5v8b.execute-api.us-east-2.amazonaws.com/default/milkdropShaderConverter';
@@ -30,7 +30,7 @@ async function _convertHLSL (text) {
   return responseBody.shader;
 }
 
-function _optimizeShader (text) {
+function _optimizeShader (optimizeGLSL, text) {
   if (text.length === 0) {
     return text;
   }
@@ -41,22 +41,18 @@ function _optimizeShader (text) {
   return optimizedShader;
 }
 
-async function _convertShader (text) {
+async function _convertShader (optimizeGLSL, text) {
   try {
     const shader = prepareShader(text);
     const convertedShader = await _convertHLSL(shader);
-    const optimizedShader = _optimizeShader(convertedShader);
+    const optimizedShader = _optimizeShader(optimizeGLSL, convertedShader);
     return optimizedShader;
   } catch (e) {
     return '';
   }
 }
 
-async function convertPreset (text) {
-  let mainPresetText = text.split('[preset00]')[1];
-  mainPresetText = mainPresetText.replace(/\r\n/g, '\n');
-
-  const presetParts = splitPreset(mainPresetText);
+function _convertPresetBase (presetParts) {
   const parsedPreset = milkdropParser.convert_preset_wave_and_shape(
     presetParts.presetVersion,
     presetParts.presetInit,
@@ -71,9 +67,30 @@ async function convertPreset (text) {
     presetParts.waves
   );
 
-  const [warpShader, compShader] = await Promise.all([
-    _convertShader(presetParts.warp),
-    _convertShader(presetParts.comp)
+  return presetMap;
+}
+
+async function convertPreset (text) {
+  let mainPresetText = text.split('[preset00]')[1];
+  mainPresetText = mainPresetText.replace(/\r\n/g, '\n');
+
+  const presetParts = splitPreset(mainPresetText);
+
+  const optimizeGLSL = await new Promise((resolve) => {
+    GLSLOptimizer().then((Module) => {
+      const optimize = Module.cwrap('optimize_glsl', 'string', [
+        'string',
+        'number',
+        'number'
+      ]);
+      resolve(optimize);
+    });
+  });
+
+  const [presetMap, warpShader, compShader] = await Promise.all([
+    _convertPresetBase(presetParts),
+    _convertShader(optimizeGLSL, presetParts.warp),
+    _convertShader(optimizeGLSL, presetParts.comp)
   ]);
 
   return Object.assign({}, presetMap, {
