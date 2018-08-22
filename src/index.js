82106893875e4b5ab879cf4a78643386b460be56
jberg <jordannealberg@gmail.com>
2018-08-22 16:20:30 -0700

    add conversion timeout

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import Worker from './convert.worker';
 
 // eslint-disable-next-line import/prefer-default-export
 export async function convertPreset (text) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     const worker = new Worker();
     worker.postMessage(text);
     worker.onmessage = (event) => {
       resolve(event.data);
     };
+    setTimeout(() => reject(new Error('Preset conversion timed out')), 15000);
   });
 }
