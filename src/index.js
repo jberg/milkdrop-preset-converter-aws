fb08836bd0fe0c59d4e9564b84ce0311c63ef8ad
jberg <jordannealberg@gmail.com>
2018-08-30 17:08:04 -0700

    fix wave/shape functions

--- src/index.js
+++ src/index.js
@@ -36,7 +36,11 @@ export function convertPresetEquations (presetVersion, initEQs, frameEQs, pixelE
 }
 
 export function convertWaveEquations (presetVersion, initEQs, frameEQs, pointEQs) {
-  const parsedPreset = milkdropParser.make_wave_map(presetVersion, initEQs, frameEQs, pointEQs);
+  const parsedPreset = milkdropParser.make_wave_map(presetVersion, {
+    init_eqs_str: initEQs,
+    frame_eqs_str: frameEQs,
+    point_eqs_str: pointEQs
+  });
   return {
     init_eqs_str: parsedPreset.perFrameInitEQs ? parsedPreset.perFrameInitEQs.trim() : '',
     frame_eqs_str: parsedPreset.perFrameEQs ? parsedPreset.perFrameEQs.trim() : '',
@@ -45,7 +49,10 @@ export function convertWaveEquations (presetVersion, initEQs, frameEQs, pointEQs
 }
 
 export function convertShapeEquations (presetVersion, initEQs, frameEQs) {
-  const parsedPreset = milkdropParser.make_shape_map(presetVersion, initEQs, frameEQs);
+  const parsedPreset = milkdropParser.make_shape_map(presetVersion, {
+    init_eqs_str: initEQs,
+    frame_eqs_str: frameEQs
+  });
   return {
     init_eqs_str: parsedPreset.perFrameInitEQs ? parsedPreset.perFrameInitEQs.trim() : '',
     frame_eqs_str: parsedPreset.perFrameEQs ? parsedPreset.perFrameEQs.trim() : ''
